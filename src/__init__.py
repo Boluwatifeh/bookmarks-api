8713ba6566e0ba4344418eede4f10cf30c9ab4e0
boluwatifeh <tifeasypeasy@gmail.com>
2022-09-20 22:51:00 +0100

    modify gigignore

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
+import os
+from src.auth import auth
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
 
-        app.config.from_mapping(SECRET_KEY="dev") 
+        app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY")) 
     
     else:
         app.config.from_mapping(test_config)
     
+    app.register_blueprint(auth)
     @app.route("/")
     def index():
         return jsonify({"message": "Hello World"})
