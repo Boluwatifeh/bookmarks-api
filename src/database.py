8e2179b4ceea19bb0ea8c576cbac45ec1e4bd614
boluwatifeh <tifeasypeasy@gmail.com>
2022-09-21 01:22:07 +0100

    create db model

---
+++ src/database.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from enum import unique
+from re import S
+import string
+import random
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False) 
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+    bookmarks = db.relationship('Bookmark', backref='user')
+
+    def __repr__(self) -> str:
+        return "User >>>>> {self.username}"
+
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text(), nullable=True)
+    url = db.Column(db.Text(), nullable=False)
+    short_url = db.Column(db.String(5), nullable=True)
+    visits = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+    
+
+    def generate_short_characters(self):
+        chars = string.digits+string.ascii_letters
+        picked_chars = ''.join(random.choices(chars, k=5))
+
+        link = self.query.filter_by(short_url=picked_chars).first()
+
+        if link:
+            self.generate_short_characters()
+        else:
+            return picked_chars
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.short_url = self.generate_short_characters()
+
+    def __repr__(self) -> str:
+        return "Bookmark >>>>> {self.url}"
+    
+
+    
