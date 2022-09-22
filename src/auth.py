d57284e484b27359ef72f844e88ed71c73bafb8e
boluwatifeh <tifeasypeasy@gmail.com>
2022-09-22 23:54:18 +0100

    modify auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from markupsafe import re
+import validators
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__ , url_prefix="/api/v1/auth")
 
 @auth.post("/register")
 def register():
-    return jsonify({"message": "user created successfully"})
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    
+    if len(username) < 4:
+        return jsonify({"message": 
+                        "username is too short! Try again"}), 400
+    if len(password) < 6:
+        return jsonify({"message": 
+                        "password is too short! Try again"}), 400
+
+    if not validators.email():
+        return jsonify({"message": 
+                        "email is invalid! Try again"}), 400
+                        
+
 
 @auth.get("/me")
 def me():
