5aecf96b5d733d213b41113a5a63834feee2f809
boluwatifeh <tifeasypeasy@gmail.com>
2022-09-28 20:23:57 +0100

    add routes to create and post bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,8 +1,68 @@
-from flask import Blueprint, jsonify
+from crypt import methods
+from operator import methodcaller
+from itsdangerous import json
+import validators
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from src.database import Bookmark
+from src.database import db
 
 bookmarks = Blueprint("bookmarks", __name__ , url_prefix="/api/v1/bookmarks")
 
 
-@bookmarks.get("/")
+@bookmarks.route("/", methods=['POST', 'GET'])
+@jwt_required()
 def get_bookmarks():
-    return jsonify({"bookmarks ": [] })
+    
+    current_user = get_jwt_identity()
+
+    if request.method == 'POST':
+        body = request.get_json().get('body', "")
+        url = request.get_json().get('url', "")
+        
+
+        if not validators.url(url):
+            return jsonify({
+                "error": "invalid url, try again!"
+            }), 400
+        
+        if Bookmark.query.filter_by(url=url).first():
+            return jsonify({
+                "error" : "Url already exists."
+            }), 409
+        bookmark = Bookmark(url=url, body=body, user_id=current_user)
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            "id": bookmark.id,
+            "body": bookmark.body,
+            "url": bookmark.url,
+            "short_url": bookmark.short_url,
+            "visits": bookmark.visits,
+            "created_at": bookmark.created_at,
+            "updated_at": bookmark.updated_at
+
+        }), 201
+    
+    else:
+        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+        
+        data = []
+
+        for bookmark in bookmarks:
+            data.append({
+                "id": bookmark.id,
+                "body": bookmark.body,
+                "url": bookmark.url,
+                "short_url": bookmark.short_url,
+                "visits": bookmark.visits,
+                "created_at": bookmark.created_at,
+                "updated_at": bookmark.updated_at
+            })
+
+        return jsonify({
+            "data": data
+        }), 200
+        
