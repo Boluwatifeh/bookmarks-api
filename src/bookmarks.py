f8d22b37782bbe4d05a6f81bbba6b68bd29b6111
boluwatifeh <tifeasypeasy@gmail.com>
2022-10-03 22:32:31 +0100

    remove unused imports

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,5 +1,3 @@
-from operator import methodcaller
-from wsgiref.util import request_uri
 import validators
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
