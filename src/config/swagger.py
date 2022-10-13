62616b6cc249a39ef0b1aae495383a6f34c8115d
boluwatifeh <tifeasypeasy@gmail.com>
2022-10-13 19:31:39 +0100

    modify swagger template

--- src/config/swagger.py
+++ src/config/swagger.py
@@ -2,8 +2,36 @@ template = {
     "swagger": "3.0",
     "info": {
         "title" : "Bookmarks API",
-        "description" : "An Api for Bookmarks"
-    }
+        "description" : "An Api for Bookmarks",
+        "contact" : {
+            "name": "Bookmarks API Support",
+            "url": "http://www.twitter.com/tife_easypeasy",
+            "email": "tifeasypeasy@gmail.com"
+        }, 
+        "license" : {
+                "name": "Apache 2.0",
+                "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
+        }, 
+        "version" : "1.0"
+    },
+    "basePath": "/api/v1",
+    "schemes": [
+        "https",
+        "http"
+    ],
+    "components": {
+        
+        "securitySchemes": {
+            "bearerAuth": {
+                "type" : "http",
+                "scheme" : "bearer",
+                "name" : "Authorization",
+                "in"   : "header",
+                "bearerFormat" : "JWT"
+                  }
 
+             }
+
+    }
 
 }
